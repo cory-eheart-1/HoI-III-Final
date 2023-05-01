8d22f629c8f018bbef0b59a0197dbee20f3e33fb
cory-eheart <cleheart@ualr.edu>
2023-04-30 23:04:21 -0500

    final

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function showAnswer(title) {
     var answer = document.getElementById(title);
     answer.style.visibility = 'visible';
+    answer.style.whiteSpace = "normal";
+    answer.style.overflow = "visible";
+    answer.style.marginBottom = "20px";
 }
